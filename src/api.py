3df1459846cebe02661bce9c73047ed2ce225382
dalet <victordalet@protonmail.com>
2023-11-30 11:43:05 +0100

    feat(request): send picture for stock data

--- src/api.py
+++ src/api.py
@@ -8,12 +8,23 @@ class Api:
         self.api_key: str = key
         self.city: str = city
         self.name_extension: str = '-not-trained'
+        self.name_extension_2_count: int = 0
         self.extension: str = 'png'
 
     def send_picture(self):
-        requests.post(self.url_api + "/add_picture_alert_or_moment", json={
-            "key": self.api_key,
-        }, files={
-            'file': open(f'{self.city}{self.name_extension}.'
-                         f'{self.extension}', 'rb')
-        })
+        try:
+            requests.post(self.url_api + "/add_picture_alert_or_moment", json={
+                "key": self.api_key,
+            }, files={
+                'file': open(f'{self.city}{self.name_extension}.'
+                             f'{self.extension}', 'rb')
+            })
+            requests.post(self.url_api + "/add_picture_alert_or_moment", json={
+                "key": self.api_key,
+            }, files={
+                'file': open(f'{self.city}{self.name_extension_2_count}.'
+                             f'{self.extension}', 'rb')
+            })
+            self.name_extension_2_count += 1
+        except Exception:
+            pass
